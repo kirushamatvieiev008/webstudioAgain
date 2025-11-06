71a4ca9277aece6c40d996c4de816f25184ec95d
Кірюша <mkiril087@gmail.com>
2025-11-06 21:58:24 +0200

    gh

--- main.js
+++ main.js
@@ -1,1 +1,57 @@
-const modalBlur = document.querySelector('.blur');
+const inpsModal = document.querySelector(".inpsModal");
+const svgsModal = document.querySelector(".svgsModal");
+const svgEmail = document.querySelector(".svgEmail");
+const svgPhone = document.querySelector(".svgPhone");
+const forjs1 = document.querySelector('.forjs1');
+const forjs2 = document.querySelector('.forjs2');
+const forjs3 = document.querySelector('.forjs3');
+const closeModalBtn = document.querySelector('.svgModal');
+const blurBlock = document.querySelector('.blur');
+const modalWindow = document.querySelector('.modalWindow');
+const openModal = document.querySelector('.contactsBtn');
+
+openModal.addEventListener('click', event => {
+    blurBlock.style.display = 'block';
+    modalWindow.style.display = 'block';
+    document.body.style.overflow = 'hidden';
+})
+
+
+forjs1.addEventListener("focus", (event) => {
+  svgsModal.style.color = "#2196F3";
+//   svgEmail.style.color = "#2196F3";
+});
+
+forjs1.addEventListener("blur", (event) => {
+  svgsModal.style.color = "#212121";
+//   svgPhone.style.color = "#212121";
+//   svgEmail.style.color = "#212121";
+});
+
+
+forjs2.addEventListener("focus", (event) => {
+  svgPhone.style.color = "#2196F3";
+});
+
+forjs2.addEventListener("blur", (event) => {
+  svgPhone.style.color = "#212121";
+});
+
+forjs3.addEventListener("focus", (event) => {
+  svgEmail.style.color = "#2196F3";
+});
+
+forjs3.addEventListener("blur", (event) => {
+  svgEmail.style.color = "#212121";
+});
+
+closeModalBtn.addEventListener('click', event => {
+    event.preventDefault()
+    // closeModalBtn.style.dislay = 'none';
+    blurBlock.style.display = 'none';
+    modalWindow.style.display = 'none';
+    document.body.style.overflow = 'visible';
+})
+
+
+modalWindow
