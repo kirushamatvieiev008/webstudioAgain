aadca8759465dd734bfe536c9b56a0a705f782b8
Кірюша <mkiril087@gmail.com>
2025-11-10 19:07:07 +0200

    gh

--- main.js
+++ main.js
@@ -1,35 +1,39 @@
-const inpsModal = document.querySelector(".inpsModal");
+const inpsModal = Array.from(document.querySelectorAll(".inpsModal"));
 const svgsModal = document.querySelector(".svgsModal");
 const svgEmail = document.querySelector(".svgEmail");
 const svgPhone = document.querySelector(".svgPhone");
-const forjs1 = document.querySelector('.forjs1');
-const forjs2 = document.querySelector('.forjs2');
-const forjs3 = document.querySelector('.forjs3');
-const closeModalBtn = document.querySelector('.svgModal');
-const blurBlock = document.querySelector('.blur');
-const modalWindow = document.querySelector('.modalWindow');
-const openModal = document.querySelector('.contactsBtn');
-const formModal = document.querySelector('.formModal');
-const checkboxModal = document.querySelector('.checkboxModal');
+const forjs1 = document.querySelector(".forjs1");
+const forjs2 = document.querySelector(".forjs2");
+const forjs3 = document.querySelector(".forjs3");
+const closeModalBtn = document.querySelector(".svgModal");
+const blurBlock = document.querySelector(".blur");
+const modalWindow = document.querySelector(".modalWindow");
+const openModal = document.querySelector(".takeOrder");
+const formModal = document.querySelector(".formModal");
+const checkboxModal = document.querySelector(".checkboxModal");
+const flexModal = document.querySelector(".flexModal");
+const stayComment = document.querySelector(".stayComment");
+const notificationbeware = document.querySelector(".notificationbeware");
+const lastCheck = document.querySelector(".lastCheck");
 
-openModal.addEventListener('click', event => {
-    blurBlock.style.display = 'block';
-    modalWindow.style.display = 'block';
-    document.body.style.overflow = 'hidden';
-})
+console.log(inpsModal);
 
+openModal.addEventListener("click", (event) => {
+  blurBlock.style.display = "block";
+  modalWindow.style.display = "block";
+  document.body.style.overflow = "hidden";
+});
 
 forjs1.addEventListener("focus", (event) => {
   svgsModal.style.color = "#2196F3";
-//   svgEmail.style.color = "#2196F3";
+  //   svgEmail.style.color = "#2196F3";
 });
 
 forjs1.addEventListener("blur", (event) => {
   svgsModal.style.color = "#212121";
-//   svgPhone.style.color = "#212121";
-//   svgEmail.style.color = "#212121";
+  //   svgPhone.style.color = "#212121";
+  //   svgEmail.style.color = "#212121";
 });
-
 
 forjs2.addEventListener("focus", (event) => {
   svgPhone.style.color = "#2196F3";
@@ -47,24 +51,58 @@ forjs3.addEventListener("blur", (event) => {
   svgEmail.style.color = "#212121";
 });
 
-closeModalBtn.addEventListener('click', event => {
-    event.preventDefault()
-    // closeModalBtn.style.dislay = 'none';
-    blurBlock.style.display = 'none';
-    modalWindow.style.display = 'none';
-    document.body.style.overflow = 'visible';
-})
-
-
-formModal.addEventListener('submit', event => {
+closeModalBtn.addEventListener("click", (event) => {
   event.preventDefault();
-  console.log(checkboxModal.value);
-  
-  if (checkboxModal.value)
-  
+  // closeModalBtn.style.dislay = 'none';
+  blurBlock.style.display = "none";
+  modalWindow.style.display = "none";
+  document.body.style.overflow = "visible";
+  notificationbeware.style.display = 'none';
+  lastCheck.style.display = 'none';
+});
 
-  // if ()
-  modalWindow.style.display = 'none';
-  blurBlock.style.display = 'none';
-  document.body.style.overflow = 'visible';
-})
+// formModal.addEventListener("submit", (event) => {
+//   event.preventDefault();
+//   // console.log(forjs3.value);
+
+//   // if ()
+//   modalWindow.style.display = "none";
+//   blurBlock.style.display = "none";
+//   document.body.style.overflow = "visible";
+// });
+
+checkboxModal.addEventListener("click", (event) => {
+  if (checkboxModal.checked === true) {
+    // const flexModalContent = flexModal.innerHTML;
+    if (
+      forjs3.value === "" ||
+      forjs2.value === "" ||
+      forjs1.value === "" ||
+      stayComment.value === ""
+    ) {
+      notificationbeware.style.display = "block";
+      checkboxModal.checked = false;
+    } else {
+      notificationbeware.style.display = "none";
+    }
+  }
+});
+
+formModal.addEventListener("submit", (event) => {
+  event.preventDefault();
+  // console.log(forjs3.value);
+
+  if (checkboxModal.checked === false) {
+    lastCheck.style.display = "block";
+  } else {
+    modalWindow.style.display = "none";
+    blurBlock.style.display = "none";
+    document.body.style.overflow = "visible";
+    inpsModal.forEach((inp) => {
+      inp.value = "";
+    });
+    stayComment.value = "";
+    lastCheck.style.display = "none";
+    checkboxModal.checked = false;
+  }
+});
